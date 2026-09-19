53356cc1302d031f44e32df4a0eba646afb8ff09
krishvam970 <krishnavamsiete@gmail.com>
2026-09-19 17:15:51 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count={}
+        for i in nums :
+            if i in count :
+                count[i]+=1
+            else:
+                count[i]=1
+        a= list(sorted(count,key=count.get,reverse=True))
+        return a[:k]
+        
